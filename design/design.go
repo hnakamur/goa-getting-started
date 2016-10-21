de5c84bcd6197e701f5ff37ca63f2b512ada8dd8
Hiroaki Nakamura <hnakamur@gmail.com>
2016-10-21 17:16:35 +0900

    Generate English YAML and overlay Japanese YAML

--- design/design.go
+++ design/design.go
@@ -6,9 +6,9 @@ import (
 )
 
 var _ = API("cellar", func() { // API defines the microservice endpoint and
-	Title("仮想ワインセラー")             // other global properties. There should be one
-	Description("シンプルなgoaサービスの例") // and exactly one API definition appearing in
-	Scheme("http")                // the design.
+	Title("The virtual wine cellar")    // other global properties. There should be one
+	Description("A simple goa service") // and exactly one API definition appearing in
+	Scheme("http")                      // the design.
 	Host("localhost:8080")
 })
 
@@ -17,10 +17,10 @@ var _ = Resource("bottle", func() { // Resources group related API endpoints
 	DefaultMedia(BottleMedia) // services.
 
 	Action("show", func() { // Actions define a single API endpoint together
-		Description("指定されたIDのボトルを取得する") // with its path, parameters (both path
-		Routing(GET("/:bottleID"))      // parameters and querystring values) and payload
-		Params(func() {                 // (shape of the request body).
-			Param("bottleID", Integer, "ボトル ID")
+		Description("Retrieve bottle with given id") // with its path, parameters (both path
+		Routing(GET("/:bottleID"))                   // parameters and querystring values) and payload
+		Params(func() {                              // (shape of the request body).
+			Param("bottleID", Integer, "Bottle ID")
 		})
 		Response(OK)       // Responses define the shape and status code
 		Response(NotFound) // of HTTP responses.
@@ -29,11 +29,11 @@ var _ = Resource("bottle", func() { // Resources group related API endpoints
 
 // BottleMedia defines the media type used to render bottles.
 var BottleMedia = MediaType("application/vnd.goa.myexample.bottle+json", func() {
-	Description("ワインボトル")
+	Description("A bottle of wine")
 	Attributes(func() { // Attributes define the media type shape.
-		Attribute("id", Integer, "唯一なボトルID")
-		Attribute("href", String, "このボトルにリクエストを送るためのAPIのhref")
-		Attribute("name", String, "ワインの名前")
+		Attribute("id", Integer, "ID of bottle")
+		Attribute("href", String, "API href of bottle")
+		Attribute("name", String, "Name of wine")
 		Required("id", "href", "name")
 	})
 	View("default", func() { // View defines a rendering of the media type.
